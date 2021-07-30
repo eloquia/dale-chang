ce85e2ec17a7adb17dd7913afcf2cd74b70c99b3
Dale Chang <dalec@Dales-MacBook-Pro.local>
2021-07-30 05:59:24 -0700

    Update SEO for Behind the Scernes

--- src/app/behind-the-scenes/behind-the-scenes.component.ts
+++ src/app/behind-the-scenes/behind-the-scenes.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-behind-the-scenes',
   templateUrl: './behind-the-scenes.component.html',
-  styleUrls: ['./behind-the-scenes.component.scss']
+  styleUrls: ['./behind-the-scenes.component.scss'],
 })
 export class BehindTheScenesComponent implements OnInit {
-
-  constructor() { }
+  constructor(private title: Title, private meta: Meta) {}
 
   ngOnInit(): void {
+    this.title.setTitle('Dale Chang - Blog');
+    // this.meta.
+    this.meta.addTags([
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'og:url', content: '/behind-the-scenes' },
+      { name: 'og:title', content: 'Dale Chang - Blog' },
+      {
+        name: 'og:description',
+        content: 'Reflections on the technology industry',
+      },
+      //   { name: 'og:image', content: '' },
+    ]);
   }
-
 }
