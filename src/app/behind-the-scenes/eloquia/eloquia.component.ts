23219a80cd2ec7f5491fdbdb3afe3f48b1268bcc
Dale Chang <dale@eloquia.io>
2021-07-30 06:06:42 -0700

    Update Eloquia components

--- src/app/behind-the-scenes/eloquia/eloquia.component.ts
+++ src/app/behind-the-scenes/eloquia/eloquia.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-eloquia',
   templateUrl: './eloquia.component.html',
-  styleUrls: ['./eloquia.component.scss']
+  styleUrls: ['./eloquia.component.scss'],
 })
 export class EloquiaComponent implements OnInit {
-
-  constructor() { }
+  constructor(private title: Title, private meta: Meta) {}
 
   ngOnInit(): void {
+    this.title.setTitle('Eloquia');
   }
-
 }
